bb7ce5daf4b80c2c0520344d616e72cfaf30dfb1
ross <rjdbcm@mail.umkc.edu>
2019-05-07 14:45:39 -0400

    Edit setup.py again..

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     packages=required_packages,
     entry_points={
         'console_scripts': [
-            'SLGR-Suite=slgr_suite.SLGR-Suite:main'
+            'SLGR<dash>Suite=slgr<underscore>suite.SLGR<dash>Suite:main'
         ]
     },
     include_package_data=True,
