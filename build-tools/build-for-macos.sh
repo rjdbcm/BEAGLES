ac04dd918302c1bb7edbce45677b6881bf50a38e
formica <42073607+rjdbcm@users.noreply.github.com>
2019-05-12 16:01:45 -0500

    typo fixes

--- build-tools/build-for-macos.sh
+++ build-tools/build-for-macos.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 # To be run from the build-tools/ directory
+# TODO: Install into a virtualenv so the binary isn't massive
+# Run this script as the super user after running:
 
-brew install python@3
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+which -s python3
+if [[ $? != 0 ]] ; then
+    # Install Python3
+    brew install python@3
+fi
 
 # clean out any old build files
 cd ../
@@ -15,12 +27,13 @@ pyinstaller -w slgrSuite.spec
 mv "dist/SLGR-Suite.app" /Applications
 
 # symlink the backend and data folders
+# Todo make these symlinks actually work inside SLGR-Suite, images load but annotations don't
 mkdir -p ~/SLGR-Suite
-sudo ln -s /Applications/SLGR-Suite.app/Contents/Resources/backend/flow /usr/sbin/flow
-sudo ln -s /Applications/SLGR-Suite.app/Contents/Resources/backend/bin/ ~/SLGR-Suite/bin/
-sudo ln -s /Applications/SLGR-Suite.app/Contents/Resources/backend/ckpt/ ~/SLGR-Suite/ckpt/
-sudo ln -s /Applications/SLGR-Suite.app/Contents/Resources/backend/built_graph/ ~/SLGR-Suite/built_graph/
-sudo ln -s /Applications/SLGR-Suite.app/Contents/Resources/backend/cfg/ ~/SLGR-Suite/cfg/
-sudo ln -s /Applications/SLGR-Suite.app/Contents/Resources/data/ ~/SLGR-Suite/data/
+sudo ln -s /Applications/SLGR-Suite.app/Contents/Resources/backend/flow /usr/local/bin/flow
+ln -s /Applications/SLGR-Suite.app/Contents/Resources/backend/bin/ ~/SLGR-Suite/bin
+ln -s /Applications/SLGR-Suite.app/Contents/Resources/backend/ckpt/ ~/SLGR-Suite/ckpt
+ln -s /Applications/SLGR-Suite.app/Contents/Resources/backend/built_graph/ ~/SLGR-Suite/built_graph
+ln -s /Applications/SLGR-Suite.app/Contents/Resources/backend/cfg/ ~/SLGR-Suite/cfg
+ln -s /Applications/SLGR-Suite.app/Contents/Resources/data/ ~/SLGR-Suite/data
 
 echo 'DONE'
