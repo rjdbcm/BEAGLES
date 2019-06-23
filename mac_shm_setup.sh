40b449e0a420cd6ca0bc98a0341ee46292001d74
formica <42073607+rjdbcm@users.noreply.github.com>
2019-06-23 12:51:32 -0500

    mac_shm_setup.sh helper script will no longer allow double mounts

--- mac_shm_setup.sh
+++ mac_shm_setup.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
-
+VOLUMEPATH="/Volumes"
 NAME="RAMDisk"
 
 function RAMDisk_mount() {
-    if [[ ! -d "Volumes/$NAME" ]]; then
+    if [[ ! -d "$VOLUMEPATH/$NAME" ]]; then
         diskutil eraseVolume HFS+ $NAME `hdiutil attach -nomount ram://$((2048 * 2))`
+    else
+        >&2 echo Error: $NAME already mounted in /Volumes
+        exit 1
     fi
 }
 
@@ -12,17 +15,17 @@ function RAMDisk_unmount() {
     while true
     do
         CURDISK=$(diskutil info RAMDisk | grep -o '/dev/disk[1-99]')
-        echo $1 $CURDISK
         if [[ "$CURDISK" = "" ]]
         then
             exit
         else
+            echo "$1 $NAME @ $CURDISK"
             hdiutil detach $CURDISK
         fi
     done
 }
 
-eval ${NAME}_${1}
+eval ${NAME}_${1} ${1}
 #if [[ "$1" = "mount" ]]; then
 #    RAMDisk_mount
 #elif [[ "$1" = "unmount" ]]; then
