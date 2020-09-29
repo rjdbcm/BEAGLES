a638550a78f787565267dca380873f39768645ec
formica <42073607+rjdbcm@users.noreply.github.com>
2020-09-29 14:36:25 -0500

    api_reference.rst added io

---
+++ beagles/io/__init__.py
@@ -0,0 +1,8 @@
+from beagles.io.flags import Flags
+from beagles.io.sharedmemory import SharedMemory
+from beagles.io.logs import get_logger
+from beagles.io.obs import TiledCaptureArray, datetime_from_filename
+from beagles.io.yolo import YoloReader, YoloWriter
+from beagles.io.labelFile import LabelFile
+from beagles.io.settings import Settings
+from beagles.io.pascalVoc import PascalVocReader, PascalVocWriter
